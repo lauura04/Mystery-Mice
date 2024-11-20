067b31cee1dc63cba21276bfd5b5d08ce391a51f
Fefpsy17 <daviddce04@gmail.com>
2024-11-20 12:23:46 +0100

    Actualización de los controles

--- js/controls.js
+++ js/controls.js
@@ -6,7 +6,8 @@ class ControlScene extends Phaser.Scene{
     preload() {
         this.load.image("backgroundcontrol", 'assets/backgroundcontrol.png');
         this.load.image("backControlButton", 'assets/backbutton.png');
-        this.load.image("staticSighttail", 'assets/SightailDialogue.png')
+        this.load.image("staticSighttail", 'assets/SightailDialogue.png');
+        this.load.image("staticScentpaw", 'assets/ScentpawDialogue.png');
     }
 
     create(){
@@ -26,6 +27,42 @@ class ControlScene extends Phaser.Scene{
         backcontrolButton.setScale(0.5);
 
         
+        const sighttailAsset = this.add.image(0.5*centerX,0.6*centerY, "staticSighttail");
+        sighttailAsset.setScale(0.4);
+
+        const controlASighttail = this.add.text(0.55*centerX, 0.65*centerY, " A ", {
+            font: '70px mousy',
+            color: '#FFFFFF',
+            backgroundColor: '#000',
+            align: 'center'
+        });
+
+        const controlWSighttail = this.add.text(0.65*centerX, 0.5*centerY, "W", {
+            font: '70px mousy',
+            color: '#FFFFFF',
+            backgroundColor: '#000',
+            align: 'center'
+        });
+
+        const controlDSighttail = this.add.text(0.72*centerX, 0.65*centerY, " D ", {
+            font: '70px mousy',
+            color: '#FFFFFF',
+            backgroundColor: '#000',
+            align: 'center'
+        });
+
+        const controlSSighttail = this.add.text(0.65*centerX, 0.8*centerY, " S ", {
+            font: '70px mousy',
+            color: '#FFFFFF',
+            backgroundColor: '#000',
+            align: 'center'
+        });
+
+        // hacer lo mismo que con sighttail para scentpaw pero con los assets de las flechas
+        const scentpawAsset = this.add.image(1.4*centerX, 0.6*centerY,"staticScentpaw" );
+        scentpawAsset.setScale(-0.45, 0.45);
+       
+        // meter lo de los poderes -> teclas por poder
 
         const textBack = this.add.text(0.2*centerX, 1.45*centerY, 'Volver', {
             font: '40px mousy',
