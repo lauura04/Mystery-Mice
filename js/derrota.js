d0abe43277568c2fac9b20d902a547ae36baf35f
Fefpsy17 <daviddce04@gmail.com>
2024-12-16 14:36:40 +0100

    Fondo derrota y arreglo mapas

--- js/derrota.js
+++ js/derrota.js
@@ -1,24 +1,24 @@
 class LoseScene extends Phaser.Scene{
-    constructor() {
-        super({ key: 'LoseScene' });
+    constructor(){
+        super({key: 'LoseScene'});
     }
 
     preload(){
         //Cargamos las imagenes que componen el fondo
-        this.load.image("fondo", 'assets/menu.png');
-        this.load.image("boton", 'assets/salir.png');
+        this.load.image("fondo", 'assets/Pantallla_Derrota.png');
+        this.load.image("botonS", 'assets/Salir.png');
     }
 
     create(){
         //variables para meter las imagenes a posteriori
-        this.centerX = this.scale.width / 2;
-        this.centerY = this.scale.height / 2;
+        const centerX = this.scale.width / 2;
+        const centerY = this.scale.height / 2;
 
         //Imagen del fondo
         const background_lose = this.add.image(centerX,centerY, "fondo");
 
         //Botón para volver al menú inicial
-        const volver = this.add.image(0.9 * centerX, 0.9 * centerY, "salir").setInteractive()
+        const volver = this.add.image(0.9 * centerX, 0.9 * centerY, "botonS").setInteractive()
             .on('pointerdown', () => {
                 this.scene.stop("LoseScene");
                 this.scene.start("IntroScene");
