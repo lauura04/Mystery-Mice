f01f9721475e79ca0c4239f0cdd822c1236d3b6e
Fefpsy17 <daviddce04@gmail.com>
2024-12-16 15:02:28 +0100

    Arreglo derrota

--- js/derrota.js
+++ js/derrota.js
@@ -5,7 +5,7 @@ class LoseScene extends Phaser.Scene{
 
     preload(){
         //Cargamos las imagenes que componen el fondo
-        this.load.image("fondo", 'assets/Pantallla_Derrota.png');
+        this.load.image("fondoD", 'assets/Pantallla_Derrota.png');
         this.load.image("botonS", 'assets/Salir.png');
     }
 
@@ -15,10 +15,17 @@ class LoseScene extends Phaser.Scene{
         const centerY = this.scale.height / 2;
 
         //Imagen del fondo
-        const background_lose = this.add.image(centerX,centerY, "fondo");
+        const background_lose = this.add.image(centerX,centerY, "fondoD");
+
+        //Texto derrota
+        const LoseText = this.add.text(0.4*centerX, 0.3*centerY, 'La alcaldesa se ha ' + 'salido con la suya',{
+            font: '70px mousy',
+            color: '#e4be9a',
+            align: 'center'
+        });
 
         //Botón para volver al menú inicial
-        const volver = this.add.image(0.9 * centerX, 0.9 * centerY, "botonS").setInteractive()
+        const volver = this.add.image(1 * centerX, 1.4 * centerY, "botonS").setInteractive()
             .on('pointerdown', () => {
                 this.scene.stop("LoseScene");
                 this.scene.start("IntroScene");
